68019e20825522b080587af7bccf3a0e6f2a1f54
8eauty <106524498+8eauty@users.noreply.github.com>
2022-08-07 17:43:21 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,61 @@
 
-import React, { Component} from "react";
+// import React, { Component} from "react";
+// import '../styles/App.css';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.cityList = [{ name: 'Goa', country: 'India' },
+//     { name: 'Amsterdam', country: 'Netherlands' },
+//     { name: 'New York', country: 'USA' },
+//     { name: 'Darjeeling', country: 'India' },
+//     { name: 'Tokyo', country: 'Japan' },
+//     { name: 'Lonavala', country: 'India' },
+//     { name: 'Brandenburg Gate', country: 'Germany' },
+//     { name: 'Reichstag Building', country: 'Germany' },
+//     { name: 'Museum Island', country: 'Germany' },
+//     { name: 'Munnar', country: 'India' },
+//     { name: 'Leh Ladakh', country: 'India' },
+//     { name: 'Goa', country: 'India' },
+//     { name: 'Agra', country: 'India' },
+//     { name: 'Dalhousie', country: 'India' },
+//     { name: 'Coorg', country: 'India' },
+//     { name: 'Rome', country: 'Italy' },
+//     { name: 'Milan', country: 'Italy' },
+//     { name: 'Venice', country: 'Italy' },
+//     { name: 'Varanasai', country: 'India' },
+//     { name: 'Jaipur', country: 'India' },
+//     { name: 'The Hofburg', country: 'Austria' },
+//     { name: 'Belvedere Palace', country: 'Austria' },
+//     { name: 'St. Stephen Cathedral', country: 'Austria' },
+//     { name: 'Kahna National Park', country: 'India' },
+//     { name: 'Amritsar', country: 'India' },
+//     { name: 'Mussoorie', country: 'India' },
+//     { name: 'Mount Abu', country: 'India' },
+//     { name: 'Tirupati', country: 'India' },
+//     ]
+//   }
+
+//   render() {
+//     return (
+//       <div id="main">
+//       <ol type="this.cityList">
+//             {this.cityList.filter((cout)=> cout.country === 
+//             'India').map((city)=>{
+//               return(
+//                 <li key="location">{city.name}</li>
+//               )
+//             })}
+//         </ol>
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default App;
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
@@ -12,41 +68,41 @@ class App extends Component {
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
     { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+ 
     ]
+
+    this.finalList = this.sort();
+}
+  getKey(index) {
+      return "location"+(index+1);
+  }
+
+  sort(){
+   
+    const indianCity = []
+     this.cityList.forEach((city) =>{
+        
+        if  (city.country === "India" && !indianCity.includes(city.name))
+        { 
+          console.log(indianCity)
+           indianCity.push(city.name)
+        }
+   })
+      
+    return indianCity;
   }
 
   render() {
+    
+
     return (
       <div id="main">
-      <ol type="this.cityList">
-            {this.cityList.filter((cout)=> cout.country === 
-            'India').map((city)=>{
-              return(
-                <li key="location">{city.name}</li>
-              )
-            })}
+        {/* Do not remove the main div */}
+        
+        <ol>
+            {this.finalList.map((city,index)=>{
+                return <li key = {this.getKey(index)}>{city} </li>
+              })}
         </ol>
       </div>
     )
@@ -55,3 +111,4 @@ class App extends Component {
 
 
 export default App;
+
